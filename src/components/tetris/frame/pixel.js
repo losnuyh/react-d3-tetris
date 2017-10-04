50437284127f27b241dc7b2b60595776937580f6
sol HYUN <solsol9057@gmail.com>
2017-10-04 18:33:27 +0900

    add id per pixel

--- src/components/tetris/frame/pixel.js
+++ src/components/tetris/frame/pixel.js
@@ -4,8 +4,23 @@ import { Rect } from 'd3';
 
 
 const Pixel = ({x, y}) => {
+    let x_id = Math.floor(x/30);
+    let y_id;
+
+
+    if((y/30)>21) {
+        y_id = Math.floor(y/30) - 2;
+    } else if ((y/30)>10) {
+        y_id = Math.floor(y/30) - 1 ;
+    } else {
+        y_id = Math.floor(y/30);
+    }
+
+    x_id++, y_id++;
+
     return (
         <rect
+          id={`px${x_id}-${y_id}`}
           x={x} y={y}
           width={30} height={30} fill={'transparent'} />
     );
